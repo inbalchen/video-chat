d27f78a18b4bd4b1d97777a28bd679f93bfdae33
inbalchen <inbal6026>
2021-01-29 01:11:34 +0200

    initial commit

---
+++ public/script.js
@@ -0,0 +1,61 @@
+const socket = io('/')
+const videoItems = document.getElementById('video-items')
+var peer = new Peer(undefined, {
+    host: '/',
+    port: '8081'
+});
+
+const myVideo = document.createElement('video');
+myVideo.muted = true
+const peers = {}
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
+
+socket.on('user-disconnected', userId => {
+    if(peers[userId]){
+        peers[userId].close()
+    }
+})
+
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+});
+
+
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoItems.append(video)
+}
